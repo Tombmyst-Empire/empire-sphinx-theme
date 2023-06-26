147ed2fdf2a28a79314394561b59119db5f20299
Yann Tremblay alt <ytremblay@rhetorik.com>
2023-06-26 14:44:49 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# 'setup.py'
+setup(
+    ...
+    entry_points = {
+        'sphinx.html_themes': [
+            'empire_theme = empire_theme',
+        ]
+    },
+    ...
+)
+
+# 'your_package.py'
+from os import path
+
+def setup(app):
+    app.add_html_theme('empire_theme', path.abspath(path.dirname(__file__)))
