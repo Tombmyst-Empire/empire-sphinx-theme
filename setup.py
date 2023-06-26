5b8929a05d34f841244d45d420471b142ad36779
Yann Tremblay alt <ytremblay@rhetorik.com>
2023-06-26 16:06:49 -0400

    fix

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 setup(
     name='empire_sphinx_theme',
     version='1.0',
-    entry_points = {
-        'sphinx.html_themes': [
-            'empire_sphinx_theme = your_package',
+    packages=["empihre_sphinx_theme"],
+    package_data={
+        "empire_sphinx_theme": [
+            "theme.conf",
+            "static/*.*",
         ]
-    }
+    },
+    entry_points={"sphinx.html_themes": ["empiretheme = empire_sphinx_theme"]},
 )
 
--- your_package.py
+++
@@ -1,5 +0,0 @@
-# 'your_package.py'
-from os import path
-
-def setup(app):
-    app.add_html_theme('empire_sphinx_theme', path.abspath(path.dirname(__file__)))
