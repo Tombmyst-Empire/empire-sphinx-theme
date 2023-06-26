27afec8f7bd81335266c8002d48e1913c556390b
Yann Tremblay alt <ytremblay@rhetorik.com>
2023-06-26 15:43:56 -0400

    your_package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     entry_points = {
         'sphinx.html_themes': [
-            'empire_sphinx_theme = empire_sphinx_theme',
+            'empire_sphinx_theme = your_package',
         ]
     }
 )
